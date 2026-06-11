2c09dd2b172dace1a1c9853e1c27dd28490b0ccc
Ayush Jaiswal <aj7984435@gmail.com>
2026-06-11 16:32:01 +0530

    Created Pricing page

--- client/src/pages/Community.tsx
+++ client/src/pages/Community.tsx
@@ -72,7 +72,7 @@ const Community = () => {
                       <span className="test-xs text-shadow-gray-500">{new Date(project.createdAt).toLocaleDateString()}</span>
                       <div className="flex gap-3 text-white text-sm">
 
-                        <button onClick={() => navigate(`/preview/${project.id}`)} className="px-3 py-1.5 bg-white/10 hover:bg-white/15 rounded-md transition-colors flex items-center gap-2">
+                        <button className="px-3 py-1.5 bg-white/10 hover:bg-white/15 rounded-md transition-colors flex items-center gap-2">
                           <span className="bg-gray-200 size-4.5 rounded-full text-black font-semibold flex items-center justify-center">{project.user?.name?.slice(0, 1)}</span>
                           {project.user?.name}
                         </button>

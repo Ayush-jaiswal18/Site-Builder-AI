7d92b6daae715d8ed3d9009d4a61389518233dcf
Ayush Jaiswal <aj7984435@gmail.com>
2026-07-19 12:05:46 +0530

    Added auth using better-auth

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const port =  3000;
@@ -12,6 +14,8 @@ const corsOption = {
 
 app.use(cors(corsOption))
 app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 
 app.get('/', (req: Request, res: Response) => {
